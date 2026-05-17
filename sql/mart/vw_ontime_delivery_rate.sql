77ef8ec3bbb99dbbad196a2d3a4672ed92931ec7
Kittin Phummarawong <146421459+kittin-phm@users.noreply.github.com>
2026-05-17 20:10:45 +0700

    Update vw_ontime_delivery_rate.sql

--- sql/mart/vw_ontime_delivery_rate.sql
+++ sql/mart/vw_ontime_delivery_rate.sql
@@ -1,4 +1,8 @@
+-- vw_ontime_delivery_rate.sql
 CREATE OR REPLACE VIEW `project-839c799e-2b34-4fae-814.olist_staging.vw_ontime_delivery_rate` AS
 SELECT
-    SAFE_DIVIDE(SUM(ontime_orders), SUM(total_delivered_orders)) * 100 AS avg_ontime_rate_pct
+    FORMAT_DATE('%Y-%m', order_date) AS year_month,
+    SAFE_DIVIDE(SUM(ontime_orders), SUM(total_delivered_orders)) * 100 AS ontime_delivery_rate_pct
 FROM `project-839c799e-2b34-4fae-814.olist_staging.mart_daily_revenue`
+GROUP BY year_month
+ORDER BY year_month
