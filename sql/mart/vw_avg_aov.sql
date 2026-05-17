426c6cf3395b15098ce078c392c75db5db76498a
Kittin Phummarawong <146421459+kittin-phm@users.noreply.github.com>
2026-05-17 20:09:04 +0700

    Update vw_avg_aov.sql

--- sql/mart/vw_avg_aov.sql
+++ sql/mart/vw_avg_aov.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW `project-839c799e-2b34-4fae-814.olist_staging.vw_avg_aov` AS
 SELECT
-    AVG(avg_order_value) AS avg_aov
+    SAFE_DIVIDE(SUM(gmv), SUM(total_orders)) AS avg_aov
 FROM `project-839c799e-2b34-4fae-814.olist_staging.mart_daily_revenue`
