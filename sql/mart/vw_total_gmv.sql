e8504c30a40a052d610368dbf1f65b384d138b20
Kittin Phummarawong <146421459+kittin-phm@users.noreply.github.com>
2026-05-17 20:11:21 +0700

    Update vw_total_gmv.sql

--- sql/mart/vw_total_gmv.sql
+++ sql/mart/vw_total_gmv.sql
@@ -1,4 +1,8 @@
+-- vw_total_gmv.sql
 CREATE OR REPLACE VIEW `project-839c799e-2b34-4fae-814.olist_staging.vw_total_gmv` AS
 SELECT
+    FORMAT_DATE('%Y-%m', order_date) AS year_month,
     SUM(gmv) AS total_gmv
 FROM `project-839c799e-2b34-4fae-814.olist_staging.mart_daily_revenue`
+GROUP BY year_month
+ORDER BY year_month
